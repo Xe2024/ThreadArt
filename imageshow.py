b06f72dc7b4daee615750b433aeb650cf14d0e20
Xe2024 <rajprasad280323@gmail.com>
2024-05-10 00:00:45 +0530

    on the way to implement the robust update thread

--- imageshow.py
+++ imageshow.py
@@ -3,15 +3,16 @@ from tkinter import ttk
 from tkinter import Tk, Frame, Label
 from PIL import Image, ImageTk
 import tkinter as  tk
-
+import math
 
 root = Tk()
-
+framenailCount = 120
 CanvasHeight =650
 CanvasWidth = 1000
 Ox = CanvasWidth/2
 Oy = CanvasHeight/2
 frameRadius = 200
+pointsList = []
 
 C = Canvas(root, bg="#2B2C2B",
            height=CanvasHeight, width=CanvasWidth)
@@ -32,7 +33,50 @@ C.create_image(Ox,Oy, image= photo)
 
 
 C.create_oval(Ox - frameRadius, Oy - frameRadius, Ox + frameRadius, Oy + frameRadius, outline='black', fill='' ,width=5)
-               
+
+#creating N-points to place on the circular frame
+'''
+we will create these points using complex number rotations , where the angle difference  = 360/N and vectors from centre of the frame to those symmetrically placed n-points will be the C.numb represented by 
+R*e**(i*(pi/180)*(360/N)). the vectors will be calculated through a loop named pointsCalculator.
+'''
+#loop for calculating points on the frame
+def pointCalculator(FRAMENAILCOUNT , OX , OY , RADIUSOFFRAME ):
+  R=RADIUSOFFRAME
+  N=FRAMENAILCOUNT
+  for i in range(N):
+     theta = ((math.pi)/180)*(360/N)
+     x_comp = R*math.cos(i*theta)
+     y_comp = R*math.sin(i*theta)
+     
+
+     a = round(x_comp,2)
+     b= round(y_comp,2)
+     Z = a + b*(1j)
+
+     # push the Z into the pointsList[]
+     pointsList.append(Z)
+
+pointCalculator(120,Ox,Oy , frameRadius)
+
+# drawing points on the frame 
+def draw_point(x, y,OX,OY, color="white"):
+  """Draws a point on the canvas.
+
+  Args:
+      x: The x-coordinate of the point.
+      y: The y-coordinate of the point.
+      color: The color of the point (defaults to black).
+  """
+  radius = 4  # Adjust radius as needed
+  C.create_oval((OX+x) - radius, (OY+y) - radius,
+                     (OX+x) + radius,( OY+y) + radius,
+                     fill=color)
+
+for i in range(framenailCount):
+  z = pointsList[i]
+  x= z.real
+  y =z.imag
+  draw_point(x,y,Ox,Oy,"white" );
  
 C.grid()
 
